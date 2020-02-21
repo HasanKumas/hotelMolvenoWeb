7a7c6ae9ac18b24d9ee1b6bd2d1480ba4c35b6af
szareian <shahin.zareian@gmail.com>
2020-02-21 15:09:38 +0100

    21-02-2020

--- src/main/java/com/hotelMolveno/hotelMolveno/controllers/RoomController.java
+++ src/main/java/com/hotelMolveno/hotelMolveno/controllers/RoomController.java
@@ -11,6 +11,7 @@ import java.util.List;
 @RestController
 @RequestMapping("api/rooms")//end point
 public class RoomController {
+
     @Autowired//connect to database
     private RoomRepository roomRepository;
 

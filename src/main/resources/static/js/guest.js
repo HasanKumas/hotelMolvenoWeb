34ce11945265d3a52c5b0f832c339fecd586c60b
HKUMAS <hsnkms75@gmail.com>
2020-02-28 11:56:18 +0100

    demo web version 1.3 28-02-2020

--- src/main/resources/static/js/guest.js
+++ src/main/resources/static/js/guest.js
@@ -28,80 +28,80 @@ function getGuests() {
 }
 
 function deleteGuests() {
-   $('#tableInputGuest').hide();
-   $("#saveButton").hide();
-   $("#editBtn").hide();
-   $("#idInput").show();
-   $("#deleteBtn").show();
-   $("#deleteInputTitle").show();
+    $('#tableInputGuest').hide();
+    $("#saveButton").hide();
+    $("#editBtn").hide();
+    $("#idInput").show();
+    $("#deleteBtn").show();
+    $("#deleteInputTitle").show();
 }
 function deleteGuest() {
-   var id = $("#idInput").val();
+    var id = $("#idInput").val();
 
-   $.ajax({
-       url: "api/guests/" + id,
-       type: "DELETE",
-       success: function() {
-           alert('We succeeded!');
-           $("#idInput").val('');
-           guestDataTable.ajax.reload();
-       },
-       error: function() {
-        alert('something went wrong!');
-       }
-   });
+    $.ajax({
+        url: "api/guests/" + id,
+        type: "DELETE",
+        success: function () {
+            alert('We succeeded!');
+            $("#idInput").val('');
+            guestDataTable.ajax.reload();
+        },
+        error: function () {
+            alert('something went wrong!');
+        }
+    });
 }
 
 function changeGuestInput() {
-        $('#tableInputGuest').show();
-        $("#editBtn").show();
-        $("#deleteInputTitle").show();
-        $("#idInput").show();
-        $("#deleteBtn").hide();
-        $("#saveButton").hide();
-//        $('#tableShowRoom').hide();
+    $('#tableInputGuest').show();
+    $("#editBtn").show();
+    $("#deleteInputTitle").show();
+    $("#idInput").show();
+    $("#deleteBtn").hide();
+    $("#saveButton").hide();
+    //        $('#tableShowRoom').hide();
 
 }
 function changeGuest() {
-   var id = $("#idInput").val();
-   var guest = {
-                 firstName:$("#firstName").val(),
-                 lastName:$("#lastName").val(),
-                 tel:$("#tel").val(),
-                 email:$("#email").val(),
-                 address:$("#address").val(),
-                 idType:$("#idType").val(),
-                 idValue:$("#idValue").val()
-   };
-   var jsonObject = JSON.stringify(guest);
+    var id = $("#idInput").val();
+    var guest = {
+        firstName: $("#firstName").val(),
+        lastName: $("#lastName").val(),
+        tel: $("#tel").val(),
+        email: $("#email").val(),
+        address: $("#address").val(),
+        idType: $("#idType").val(),
+        idValue: $("#idValue").val()
+    };
+    var jsonObject = JSON.stringify(guest);
 
-   $.ajax({
-      url: "api/guests/"+id,
-      type: "PUT",
-      contentType: "application/json",
-      data: jsonObject,
-      success: function () {
-         alert('We succeeded!');
-         $("#firstName").val('');
-         $("#lastName").val('');
-         $("#tel").val('');
-         $("#email").val('');
-         $("#address").val('');
-         $("#idType").val('');
-         $("#idValue").val('');
-          guestDataTable.ajax.reload();
-      },
-      error: function () {
-          alert('try again');
-      }
-   });
+    $.ajax({
+        url: "api/guests/" + id,
+        type: "PUT",
+        contentType: "application/json",
+        data: jsonObject,
+        success: function () {
+            alert('We succeeded!');
+            $("#firstName").val('');
+            $("#lastName").val('');
+            $("#tel").val('');
+            $("#email").val('');
+            $("#address").val('');
+            $("#idType").val('');
+            $("#idValue").val('');
+            guestDataTable.ajax.reload();
+        },
+        error: function () {
+            alert('try again');
+        }
+    });
 }
 
 $(document).ready(function () {
-        getGuests();
-//
-       $("#deleteButton").click(deleteGuests);
-       $("#deleteBtn").click(deleteGuest);
-       $("#editInputButton").click(changeGuestInput);
-       $("#editBtn").click(changeGuest);
+    getGuests();
+    //
+    $("#deleteButton").click(deleteGuests);
+    $("#deleteBtn").click(deleteGuest);
+    $("#editInputButton").click(changeGuestInput);
+    $("#editBtn").click(changeGuest);
 });
